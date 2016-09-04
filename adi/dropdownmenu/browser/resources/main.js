bedfda429b1be28e339520094f49fd6c9f65d426
Ida Ebkes <contact@ida-ebkes.eu>
2016-09-04 20:00:48 +0200

    Only show arrow if item has children.

--- adi/dropdownmenu/browser/resources/main.js
+++ adi/dropdownmenu/browser/resources/main.js
@@ -1,14 +1,14 @@
 function addSubmenuButtons() {
 // If a list has child-lists, add a button for dropdown-click.
   // For each list:
-  $('#portlets-in-header .portletNavigationTree ul').each(function() {
+  $('#portlets-in-header .portletNavigationTree li').each(function() {
     var llist = $(this)
     // Check for sublist:
     var sublist = llist.find('ul')
     if(sublist.length > 0) {
       // Add button:
       $('<a href="#" alt="Open submenu" class="submenuButton openSubmenu">V</a>')
-      .insertBefore(llist.find('> li > div'))
+      .insertBefore(llist.find('> div'))
       .css('float', 'right')
       .click(function(eve) {
         handleSubmenuButtonClick(eve)
